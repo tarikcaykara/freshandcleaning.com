7ea15ee5ec702e44151b01ac16b6f46508600e7d
Tarik Caykara <tarik@caykara.com>
2026-02-09 11:04:42 +0100

    feat(ui): add Header component with contact info top bar (#2)
    * style(global): fix indentation from 4 spaces to 2 spaces
    
    * feat(ui): add Header component with contact info top bar

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return <header>
+    <div className="bg-primary text-white text-sm py-4">
+        <div className="max-w-7xl mx-auto px-4 flex justify-start items-center">
+            <div className="flex gap-6">
+                <span>0552 534 96 21</span>
+                <span>info@freshandclean.com</span>
+            </div>
+        </div>
+    </div>
+  </header>;
+}
